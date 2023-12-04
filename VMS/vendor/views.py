3d1eaf26549f5f081f56e0438b9b1dc6c34fe095
nobody-27 <nobodypal27@gmail.com>
2023-12-05 02:14:06 +0530

    added api folder

--- VMS/vendor/views.py
+++ VMS/vendor/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class VenderProfile(APIView):
+    
+    def get(self,request):
+        """
+        List all vendors.
+        """
+        # GET /api/vendors/{vendor_id}/: Retrieve a specific vendor's details.
+        return
+    
+    def post(self,request):
+        """
+        : Create a new vendor..
+        """
+        pass
+
+    def put(self,request):
+        return
+
+    def delete(self,request):
+        pass
+
+
+class Purchase(APIView):
+    
